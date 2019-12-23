90a33e92a75b5331d8c6ded8e74d597052976ad1
sovan <sovanshahihero@gmail.com>
2019-12-23 19:05:28 +0545

    Updates

---
+++ precache-manifest.e9a9062b54d848150aa35044539f0221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b88a79bc278a2c39e7dfc571e3e001",
+    "url": "/iq/index.html"
+  },
+  {
+    "revision": "520b22077e850639e9a7",
+    "url": "/iq/static/css/main.7e8aa33c.chunk.css"
+  },
+  {
+    "revision": "1438e1d978ae5764faf2",
+    "url": "/iq/static/js/2.3335eec0.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/iq/static/js/2.3335eec0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "520b22077e850639e9a7",
+    "url": "/iq/static/js/main.151bed70.chunk.js"
+  },
+  {
+    "revision": "77a5166c4543aaecd7ce",
+    "url": "/iq/static/js/runtime-main.a5c19fdb.js"
+  }
+]);
